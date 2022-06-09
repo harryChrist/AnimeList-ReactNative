985dd5178c76fc3bc68926677c2d6a3907c5bb6e
Harry <henriquecbgamer@gmail.com>
2022-06-08 21:04:44 -0300

    Language

--- data/language.js
+++ data/language.js
@@ -1,12 +1,13 @@
-import { AuthContext } from "../context/context";
-
 import pt_br from './Languages/pt_br.json'
 import en_us from './Languages/en_us.json'
+import es_es from './Languages/es_es.json'
 
 export const i18n = (key, lang) => {
     switch (lang) {
         case 'pt_br':
             return pt_br[key]
+        case 'es_es':
+            return es_es[key]
         default:
             return en_us[key]
     }
@@ -20,5 +21,9 @@ export const LangList = [
     {
         lang: "pt_br",
         name: 'Português',
+    },
+    {
+        lang: "es_es",
+        name: 'Espanish',
     }
 ]
